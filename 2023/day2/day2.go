9eac44c250df815abb12fd071b27e4c1126b9888
Mattias Mucherie <mattias.mucherie@netlight.com>
2023-12-02 16:31:00 +0100

    feat: day2

--- 2023/day2/day2.go
+++ 2023/day2/day2.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 	"time"
 )
 
-const inputFile = "test_a.txt"
+const inputFile = "input.txt"
 
 func main() {
 	partA()
+	partB()
 }
 
 func partA() {
@@ -20,10 +25,38 @@ func partA() {
 		return
 	}
 	defer file.Close()
+	max := [3]int{14, 12, 13}
+	sum := 0
+	scanner := bufio.NewScanner(file)
 
-	// day 2 part A
+	for scanner.Scan() {
+		gamePossible := true
+		line := scanner.Text()
+		parts := strings.Split(line, ":")
+		pattern := `\d+`
+		re := regexp.MustCompile(pattern)
+		gameId, _ := strconv.Atoi(re.FindString(parts[0]))
+		colorPattern := [3]string{`(\d+) blue`, `(\d+) red`, `(\d+) green`}
 
-	fmt.Println("day2 part A answer is:", nil)
+		for i, p := range colorPattern {
+			colorRe := regexp.MustCompile(p)
+			colorMatch := colorRe.FindAllStringSubmatch(line, -1)
+
+			for _, c := range colorMatch {
+				colorCount, _ := strconv.Atoi(c[1])
+
+				if colorCount > max[i] {
+					gamePossible = false
+				}
+			}
+		}
+
+		if gamePossible {
+			sum += gameId
+		}
+	}
+
+	fmt.Println("day2 part A answer is:", sum)
 	duration := time.Since(start)
 	fmt.Println(duration)
 }
@@ -37,9 +70,35 @@ func partB() {
 	}
 	defer file.Close()
 
-	// day 2 part B
+	sum := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		colorPattern := [3]string{`(\d+) blue`, `(\d+) red`, `(\d+) green`}
+		mininumColor := [3]int{0, 0, 0}
 
-	fmt.Println("day2 part B answer is:", nil)
+		for i, p := range colorPattern {
+			colorRe := regexp.MustCompile(p)
+			colorMatch := colorRe.FindAllStringSubmatch(line, -1)
+
+			for _, c := range colorMatch {
+				colorCount, _ := strconv.Atoi(c[1])
+
+				if colorCount > mininumColor[i] {
+					mininumColor[i] = colorCount
+				}
+			}
+		}
+
+		product := 1
+		for _, value := range mininumColor {
+			product *= value
+		}
+		sum += product
+
+	}
+
+	fmt.Println("day2 part B answer is:", sum)
 	duration := time.Since(start)
 	fmt.Println(duration)
 }
